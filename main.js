cfe630b0108d4dcc0c62db16d4975edaf142d2a6
TheDynamiClint <69874342+TheDynamiClint@users.noreply.github.com>
2022-10-26 13:19:07 -1000

    Style margin top and add contact modal

--- main.js
+++ main.js
@@ -168,3 +168,64 @@
     setTimeout(clearClass, 2000);
   
   })()
+
+/*
+* ADA Compliance/prevent tab out for modal
+*/
+
+
+// add all the elements inside modal which you want to make focusable
+const  focusableElements =
+'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+const modal = document.querySelector('#exampleModal'); // select the modal by it's id
+
+const firstFocusableElement = modal.querySelectorAll(focusableElements)[0]; // get first element to be focused inside modal
+const focusableContent = modal.querySelectorAll(focusableElements);
+const lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
+
+
+document.addEventListener('keydown', function(e) {
+
+  let isTabPressed = e.key === 'Tab' || e.key === 9;
+
+  if (!isTabPressed) {
+
+    return;
+
+  }
+
+  if (e.shiftKey) { // if shift key pressed for shift + tab combination
+  
+    if (document.activeElement === firstFocusableElement) {
+
+      lastFocusableElement.focus(); // add focus for the last focusable element
+      e.preventDefault();
+
+    }
+
+  } else { // if tab key is pressed
+
+    if (document.activeElement === lastFocusableElement) { 
+
+      // if focused has reached to last focusable element then focus first focusable element after pressing tab
+      firstFocusableElement.focus(); // add focus for the first focusable element
+      e.preventDefault();
+
+    }
+  }
+
+});
+
+firstFocusableElement.focus();
+
+/*
+* Bootstrap override for autofocus on modal
+*/
+
+
+var myModal = document.getElementById('contactModal')
+var myInput = document.getElementById('contactInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
